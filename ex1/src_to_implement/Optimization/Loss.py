30e5c47c84202b6da4dd7bca4b0a80ba2742da86
jkavya0 <jkavya089@gmail.com>
2025-06-03 20:26:06 +0200

    update

--- ex1/src_to_implement/Optimization/Loss.py
+++ ex1/src_to_implement/Optimization/Loss.py
@@ -1,36 +1,34 @@
 import numpy as np
-from Layers.Base import BaseLayer
 
-
-class CrossEntropyLoss(BaseLayer):
+class CrossEntropyLoss:
 
     def __init__(self):
         
-        super().__init__()
-        
         self.input_tensor = None
         self.label_tensor = None
-        
         self.forward_output = None
         self.backward_output = None
-        
 
     def forward(self, prediction_tensor, label_tensor):
-        
-
+        """
+        Computes the cross-entropy loss accumulated over the batch.
+        """
         self.label_tensor = label_tensor
+
+        # Add epsilon to avoid log(0)
+        eps = np.finfo(float).eps
+        self.input_tensor = prediction_tensor + eps
         
-        # Epsilon is added to avoid log(0)=undefined
-        self.input_tensor = np.finfo(float).eps + prediction_tensor 
-        self.forward_output = np.sum(-np.log(np.sum(self.input_tensor * label_tensor, axis=1)))
+        # Element-wise product to select predicted class probabilities, then log and sum
+        log_probs = np.log(np.sum(self.input_tensor * label_tensor, axis=1))
+        self.forward_output = -np.sum(log_probs)
 
         return self.forward_output
-    
 
     def backward(self, label_tensor):
-        
-      
-
-        self.backward_output = - (label_tensor / self.input_tensor)
-        
+        """
+        Computes the gradient of cross-entropy loss w.r.t the input.
+        """
+        eps = np.finfo(float).eps
+        self.backward_output = - (label_tensor / (self.input_tensor + eps))
         return self.backward_output
