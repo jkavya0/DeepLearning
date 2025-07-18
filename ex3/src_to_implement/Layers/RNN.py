13f22c862f0b93a2e7d622e5443442a31beff4a2
jkavya0 <jkavya089@gmail.com>
2025-07-18 18:19:26 +0200

    Update code

--- ex3/src_to_implement/Layers/RNN.py
+++ ex3/src_to_implement/Layers/RNN.py
@@ -1,204 +1,168 @@
 import numpy as np
-from Layers import TanH, Sigmoid, FullyConnected
-from Layers.Base import BaseLayer  # BaseLayer is the base class for all layers
-import warnings
 import copy
+from Layers.Base import BaseLayer
+from Layers.FullyConnected import FullyConnected
+from Layers.TanH import TanH
+from Layers.Sigmoid import Sigmoid
 
-# Simple default weight initializer (Xavier uniform)
 class DefaultWeightsInitializer:
     def initialize(self, shape, fan_in, fan_out):
         limit = np.sqrt(6 / (fan_in + fan_out))
         return np.random.uniform(-limit, limit, size=shape)
 
-# Simple default bias initializer (zeros)
 class DefaultBiasInitializer:
     def initialize(self, shape, fan_in, fan_out):
         return np.zeros(shape)
 
-
 class RNN(BaseLayer):
-
     def __init__(self, input_size, hidden_size, output_size):
-        
         super().__init__()
-        self.norm_sum = 0
-        self.hidden_state_int = None
-        self.gradient_weights_y = None
-        self.optimizer = None
-        self._optimizer2 = None
-        self.h_value = None
-        self.input_size = input_size
+        self.input_size  = input_size
         self.hidden_size = hidden_size
         self.output_size = output_size
 
-        # Initialize fully connected layers
-        self.h_layer = FullyConnected.FullyConnected(self.input_size + self.hidden_size, self.hidden_size)
-        self.y_layer = FullyConnected.FullyConnected(self.hidden_size, self.output_size)
+        self.h_layer = FullyConnected(input_size + hidden_size, hidden_size)
+        self.y_layer = FullyConnected(hidden_size, output_size)
 
-        self._optimizer = None
-        self.delta_h = None
-        self.delta_y = None
-        self.backward_output = None
-        self.error_tensor = None
+        self.tanh    = TanH()
+        self.sigmoid = Sigmoid()
 
-        self.time_step = None
-        self._weights = None
-        self.forward_output = np.array([])
-        self._gradient_weights = None
         self._memorize = False
-
-        self.tanH = TanH.TanH()
-        self.sigmoid = Sigmoid.Sigmoid()
-
-        self.trainable = True
-
-        self.batch_size = None
-
-        self.input_tensor = None
         self.hidden_state = None
 
-        # Removed calling initialize() here to allow passing initializers explicitly before initialization.
+        self.forward_output = None
+        self.input_tensor  = None
+        self.time_step     = 0
+
+        self._gradient_weights  = None
+        self.gradient_weights_y = None
+
+        self._optimizer = None
+
+        self.trainable = True
 
     @property
     def memorize(self):
         return self._memorize
 
     @memorize.setter
-    def memorize(self, memorize_value):
-        self._memorize = memorize_value
+    def memorize(self, flag):
+        self._memorize = flag
 
     @property
     def optimizer(self):
         return self._optimizer
 
     @optimizer.setter
-    def optimizer(self, optimizer_value):
-        self._optimizer = optimizer_value
-        self._optimizer1 = copy.deepcopy(optimizer_value)
-        self._optimizer2 = copy.deepcopy(optimizer_value)
+    def optimizer(self, opt):
+        self._optimizer = opt
+        # inject into sub‑layers
+        self.h_layer.optimizer = copy.deepcopy(opt)
+        self.y_layer.optimizer = copy.deepcopy(opt)
 
     @property
     def weights(self):
         return self.h_layer.weights
 
     @weights.setter
-    def weights(self, weights):
-        self.h_layer.weights = weights
+    def weights(self, w):
+        self.h_layer.weights = w
 
     @property
     def gradient_weights(self):
         return self._gradient_weights
 
     @gradient_weights.setter
-    def gradient_weights(self, value):
-        self._gradient_weights = value
-
-    def forward(self, input_tensor):
-
-        self.input_tensor = input_tensor
-        self.time_step = input_tensor.shape[0]
-
-        self.forward_output = np.zeros((self.time_step, self.output_size))
-
-        # Memorize the hidden state
-        if not self.memorize or self.hidden_state is None:
-            self.hidden_state = np.zeros((self.time_step, self.hidden_size))
-            self.hidden_state_int = np.zeros((1, self.hidden_size))
-
-        for i in range(self.time_step):
-
-            x_new = np.concatenate((
-                input_tensor[i].reshape(input_tensor[i].size, 1),
-                self.hidden_state_int.reshape(self.hidden_state[i, :].size, 1)
-            )).T
-
-            # Calculate the output of h layer
-            self.h_value = self.h_layer.forward(x_new)
-            self.hidden_state_int = self.tanH.forward(self.h_value.T).reshape(self.hidden_state[i, :].size)
-            self.hidden_state[i, :] = self.hidden_state_int
-
-            # Calculate the output of y layer
-            y_value = self.y_layer.forward(self.hidden_state[i, :].reshape(1, self.hidden_state[i, :].size))
-            self.forward_output[i, :] = self.sigmoid.forward(y_value)
-
-            if self.optimizer is not None and self.optimizer.regularizer is not None:
-                self.norm_sum = self.optimizer.regularizer.norm(self.weights)
-
-        return self.forward_output
-
-    def backward(self, error_tensor):
-
-        self.error_tensor = error_tensor
-
-        self.gradient_weights = 0
-        self.gradient_weights_y = 0
-        self.backward_output = np.zeros((self.time_step, self.input_size))
-        h_next = 0
-
-        for i in range(self.time_step - 1, -1, -1):
-
-            # Calculate the Loss of y with respect to the sigmoid function
-            self.sigmoid.forward_output = self.forward_output[i, :]
-            Loss_y = self.sigmoid.backward(self.error_tensor[i, :])
-
-            # Prepare input tensor for y_layer backward
-            self.y_layer.input_tensor = np.concatenate((
-                self.hidden_state[i, :],
-                np.ones(1)
-            )).reshape(1, self.hidden_state[i, :].size + 1)
-
-            # Backprop through y_layer
-            self.delta_y = self.y_layer.backward(Loss_y.reshape(1, Loss_y.size))
-
-            # Backprop through tanh
-            self.tanH.forward_output = self.hidden_state[i, :]
-            Loss_h = self.tanH.backward(self.delta_y + h_next)
-
-            # Prepare input tensor for h_layer backward
-            if i == 0:
-                tmp_value = np.concatenate((
-                    self.input_tensor[i, :],
-                    np.zeros(self.hidden_size)
-                )).reshape(1, self.input_tensor[i, :].size + self.hidden_size)
-            else:
-                tmp_value = np.concatenate((
-                    self.input_tensor[i, :],
-                    self.hidden_state[i-1, :]
-                )).reshape(1, self.input_tensor[i, :].size + self.hidden_state[i, :].size)
-
-            self.h_layer.input_tensor = np.concatenate((tmp_value, np.ones((tmp_value.shape[0], 1))), axis=1)
-
-            # Backprop through h_layer
-            self.delta_h = self.h_layer.backward(Loss_h)
-
-            # Accumulate gradients for all timesteps
-            self.gradient_weights += self.h_layer.gradient_weights
-            self.gradient_weights_y += self.y_layer.gradient_weights
-
-            # Calculate error propagated to input
-            self.backward_output[i, :] = self.delta_h[:, 0:self.input_size]
-
-            h_next = self.delta_h[:, self.input_size:self.input_size + self.hidden_size]
-
-        if self.optimizer is not None:
-            # Apply optimizer update to weights
-            self.h_layer.weights = self.optimizer.calculate_update(self.h_layer.weights, self.gradient_weights)
-            self.y_layer.weights = self.optimizer.calculate_update(self.y_layer.weights, self.gradient_weights_y)
-
-        return self.backward_output
+    def gradient_weights(self, gw):
+        self._gradient_weights = gw
 
     def initialize(self, weights_initializer=None, bias_initializer=None):
-
-        # Provide default initializers if none supplied
         if weights_initializer is None:
             weights_initializer = DefaultWeightsInitializer()
         if bias_initializer is None:
             bias_initializer = DefaultBiasInitializer()
 
-        # Initialize the weights of the h layer
         self.h_layer.initialize(weights_initializer, bias_initializer)
-        # Initialize the weights of the y layer
         self.y_layer.initialize(weights_initializer, bias_initializer)
+        return self.h_layer.weights
 
-        self.weights = self.h_layer.weights
-        return self.weights
+    def forward(self, input_tensor):
+        self.input_tensor  = input_tensor
+        self.time_step     = input_tensor.shape[0]
+        self.forward_output = np.zeros((self.time_step, self.output_size))
+
+        # reset or carry state
+        if not self.memorize or self.hidden_state is None:
+            self.hidden_state = np.zeros((self.time_step, self.hidden_size))
+            h_prev = np.zeros((1, self.hidden_size))
+        else:
+            h_prev = self.hidden_state[-1:].copy()
+
+        for t in range(self.time_step):
+            x_t = input_tensor[t].reshape(1, -1)
+            xh = np.hstack([x_t, h_prev])     # shape (1, in+hidden)
+
+            # store forward pass to cache input_tensor, etc.
+            h_lin = self.h_layer.forward(xh)
+            h_act = self.tanh.forward(h_lin)
+            self.hidden_state[t:t+1, :] = h_act
+            h_prev = h_act
+
+            y_lin = self.y_layer.forward(h_act)
+            y_act = self.sigmoid.forward(y_lin)
+            self.forward_output[t, :] = y_act
+
+        return self.forward_output
+
+    def backward(self, error_tensor):
+        # initialize accumulators
+        self._gradient_weights  = np.zeros_like(self.h_layer.weights)
+        self.gradient_weights_y = np.zeros_like(self.y_layer.weights)
+        dx = np.zeros((self.time_step, self.input_size))
+        dh_next = np.zeros((1, self.hidden_size))
+
+        for t in reversed(range(self.time_step)):
+            # --- Re-run forward at timestep t to repopulate caches ---
+            #  (required for correct gradient-check behavior)
+            x_t = self.input_tensor[t].reshape(1, -1)
+            if t == 0:
+                h_prev = np.zeros((1, self.hidden_size))
+            else:
+                h_prev = self.hidden_state[t-1:t, :]
+
+            xh = np.hstack([x_t, h_prev])
+            # forward through h-layer and tanh
+            h_lin = self.h_layer.forward(xh)
+            h_act = self.tanh.forward(h_lin)
+            # forward through y-layer and sigmoid
+            y_lin = self.y_layer.forward(h_act)
+            y_act = self.sigmoid.forward(y_lin)
+            # ----------------------------------------------------------------
+
+            # 1) output backprop
+            dy = self.sigmoid.backward(error_tensor[t].reshape(1, -1))
+            dh_from_y = self.y_layer.backward(dy)
+            self.gradient_weights_y += self.y_layer.gradient_weights
+
+            # 2) add carry from next step
+            dh_total = dh_from_y + dh_next
+
+            # 3) hidden backprop
+            dh_lin = self.tanh.backward(dh_total)
+            dxh    = self.h_layer.backward(dh_lin)
+            self._gradient_weights += self.h_layer.gradient_weights
+
+            # 4) split into dx and dh_next
+            dx[t, :]  = dxh[:, : self.input_size]
+            dh_next   = dxh[:, self.input_size :]
+
+        # optimizer update
+        if self._optimizer is not None:
+            self.h_layer.weights = self._optimizer.calculate_update(
+                self.h_layer.weights, self._gradient_weights
+            )
+            self.y_layer.weights = self._optimizer.calculate_update(
+                self.y_layer.weights, self.gradient_weights_y
+            )
+
+        return dx
--- ex3/src_to_implement/NeuralNetwork.py
+++ ex3/src_to_implement/NeuralNetwork.py
@@ -2,19 +2,20 @@ import copy
 
 class NeuralNetwork:
     """
-    A class representing a basic feedforward neural network. It supports training 
-    via forward and backward propagation and handles both trainable and non-trainable layers.
+    A class representing a basic feedforward neural network. It supports
+    training via forward/backward propagation, handles dropout/batch‑norm
+    phases, and includes L1/L2 regularization in the loss.
     """
 
     def __init__(self, optimizer_value, weights_initializer, bias_initializer):
-        self.weights_initializer = weights_initializer
-        self.bias_initializer = bias_initializer
-        self.optimizer = optimizer_value
-        self.loss = list()
-        self.data_layer = None
-        self.layers = list()
-        self.loss_layer = None
-        self._phase = 'train'  # Default phase
+        self.weights_initializer = copy.deepcopy(weights_initializer)
+        self.bias_initializer    = copy.deepcopy(bias_initializer)
+        self.optimizer           = optimizer_value
+        self.loss                 = []
+        self.data_layer          = None
+        self.layers              = []
+        self.loss_layer          = None
+        self._phase              = 'train'  # 'train' or 'test'
 
     @property
     def phase(self):
@@ -22,58 +23,77 @@ class NeuralNetwork:
 
     @phase.setter
     def phase(self, value):
-        """
-        Set the phase of the network and all layers ('train' or 'test').
-        """
-        if value not in ['train', 'test']:
-            raise ValueError("Phase must be either 'train' or 'test'")
-        
+        if value not in ('train', 'test'):
+            raise ValueError("Phase must be 'train' or 'test'")
         self._phase = value
-
-        # Set the phase for all layers
+        # propagate to all layers
         for layer in self.layers:
-            layer.phase = value  
-
-        # Set the phase for the loss layer if it exists
-        if self.loss_layer is not None:
-            self.loss_layer.phase = value  
-
-    def forward(self):
-
-        input_tensor, self.label_tensor = self.data_layer.next()
-
-        for layer in self.layers:
-            input_tensor = layer.forward(input_tensor)
-        loss_value = self.loss_layer.forward(input_tensor, self.label_tensor)
-
-        self.loss.append(loss_value)
-        return loss_value
-
-    def backward(self):
-
-        gradient = self.loss_layer.backward(self.label_tensor)
-
-        for layer in reversed(self.layers):
-            gradient = layer.backward(gradient)
+            layer.testing_phase = (value == 'test')
+        # also set on loss layer if it uses a phase
+        if hasattr(self.loss_layer, 'testing_phase'):
+            self.loss_layer.testing_phase = (value == 'test')
 
     def append_layer(self, layer):
-
+        """
+        Add a layer to the network. If it's trainable, give it its own optimizer
+        and initialize its parameters.
+        """
         if layer.trainable:
             layer.optimizer = copy.deepcopy(self.optimizer)
             layer.initialize(self.weights_initializer, self.bias_initializer)
-
         self.layers.append(layer)
 
-    def train(self, iterations):
-        
+    def forward(self):
+        """
+        Perform a training‐mode forward pass on the next batch from data_layer,
+        summing data loss and regularization penalty.
+        """
+        # get next batch
+        input_tensor, label_tensor = self.data_layer.next()
+        self.label_tensor = label_tensor
+
+        # ensure training mode
         self.phase = 'train'
-        for i in range(iterations):
+
+        # forward through layers, accumulating reg loss
+        reg_loss = 0.0
+        activations = input_tensor
+        for layer in self.layers:
+            activations = layer.forward(activations)
+            if self.optimizer.regularizer is not None and layer.trainable:
+                # .norm() returns L1 or L2 penalty
+                reg_loss += self.optimizer.regularizer.norm(layer.weights)
+
+        # data loss
+        data_loss = self.loss_layer.forward(activations, label_tensor)
+        total_loss = data_loss + reg_loss
+
+        # record and return
+        self.loss.append(total_loss)
+        return total_loss
+
+    def backward(self):
+        """
+        Perform backpropagation from the loss layer through all trainable layers.
+        """
+        grad = self.loss_layer.backward(self.label_tensor)
+        for layer in reversed(self.layers):
+            grad = layer.backward(grad)
+
+    def train(self, iterations):
+        """
+        Train for the given number of iterations.
+        """
+        for _ in range(iterations):
             self.forward()
             self.backward()
 
     def test(self, input_tensor):
-        
+        """
+        Run an inference pass in test mode (dropout off, batch‑norm fixed).
+        """
         self.phase = 'test'
+        activations = input_tensor
         for layer in self.layers:
-            input_tensor = layer.forward(input_tensor)
-        return input_tensor
+            activations = layer.forward(activations)
+        return activations
